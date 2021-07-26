195188f21556960451f9c8c5990831efbfb9963f
Eber Alejo <eber4202@hotmail.com>
2021-07-25 20:56:42 -0500

    add database

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # website-AppSalon
 Project about a barber shop
 
+Configure you database in `includes/database.php`
+
--- src/js/app.js
+++ src/js/app.js
@@ -84,12 +84,16 @@ function cambiarSeccion() {
 
 async function mostrarServicios() {
    try {
-      const resultado = await fetch('./servicios.json');
+
+      const url = 'http://localhost:8000/servicios.php';
+
+      const resultado = await fetch(url);
       const db = await resultado.json();
-      const {servicios} = db;
+
+      //const {servicios} = db;
 
       //generar html
-      servicios.forEach( servicio => {
+      db.forEach( servicio => {
          const {id, nombre, precio} = servicio;
 
          //DOM Scripting
